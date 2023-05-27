673c7ebe991ee656f94e58455f4d91916adc450d
Ruturajpal <98735716+Ruturajpal@users.noreply.github.com>
2023-05-27 19:23:25 +0530

    Add files via upload

---
+++ src/entity/Employee.java
@@ -0,0 +1,76 @@
+package com.employeemanagement.entity;
+
+
+public class Employee {
+
+	private int id;
+	private String first_name;
+	private String last_name;
+	private String email;
+	private String doj;
+	private String phoneno;
+	private int salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDoj() {
+		return doj;
+	}
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+	public String getPhoneno() {
+		return phoneno;
+	}
+	public void setPhoneno(String phoneno) {
+		this.phoneno = phoneno;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public Employee(int id, String first_name, String last_name, String email, String doj, String phoneno, int salary) {
+		super();
+		this.id = id;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.doj = doj;
+		this.phoneno = phoneno;
+		this.salary = salary;
+	}
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", first_name=" + first_name + ", last_name=" + last_name + ", email=" + email
+				+ ", doj=" + doj + ", phoneno=" + phoneno + ", salary=" + salary + "]";
+	}
+	
+	
+}
